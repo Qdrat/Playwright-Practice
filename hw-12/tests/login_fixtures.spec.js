5525fc9ebf428e286801e1044b80ee512b98f64a
Qdrat <alfredhager0420@gmail.com>
2026-07-07 18:45:24 +0300

    move files

--- hw-12/tests/login_fixtures.spec.js
+++ hw-12/tests/login_fixtures.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from '../fixtures.js'
+import { test, expect } from '../fixtures'
 
 test('with fixtures', async ({ loginPage, page }) => {
     await loginPage.login('standard_user', 'secret_sauce')
