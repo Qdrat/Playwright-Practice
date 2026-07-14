24b2c6c4204839ce4267200ef0f0d7b83f4ee010
Qdrat <alfredhager0420@gmail.com>
2026-07-14 15:33:59 +0300

    add hw14-15

--- playwright.config.js
+++ playwright.config.js
@@ -21,13 +21,15 @@ export default defineConfig({
     /* Retry on CI only */
     retries: process.env.CI ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.CI ? 2 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [['line'], ['allure-playwright']],
+    reporter: [['html'], ['allure-playwright']],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('')`. */
         baseURL: 'https://www.saucedemo.com',
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
         headless: true,
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
@@ -53,7 +55,7 @@ export default defineConfig({
 
         /* Test against mobile viewports. */
         {
-            name: 'Mobile Chrome',
+            name: 'pixel5',
             use: { ...devices['Pixel 5'] },
         },
         // {
